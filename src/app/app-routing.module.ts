b5c0156092ccb39895f43c26e4cc44784c9c096e
user <user@eze1-lmc-b02825.local>
2022-02-21 11:11:54 -0300

    parte 1 y 2 de ng users practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from '@core/components/home/home.component';
+import { PageNotFoundComponent } from '@core/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    PageNotFoundComponent,
+    TopnavComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    TopnavComponent,
   ]
 })
-export class CoreModule { }
+export class CoreModule {}
